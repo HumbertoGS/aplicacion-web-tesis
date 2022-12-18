b3c6c86f7d1a8eff10bba75fb297d24e7de5523c
FernandaHela <maria.herreral@ug.edu.ec>
2022-12-18 17:42:22 -0500

    se agrega menu

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import { Button } from "react-bootstrap";
 import "./App.css";
+import ColorSchemesExample from "./pages/navegacion";
 
 let data = { value: "Veces presionado: " };
 
@@ -15,12 +16,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+<ColorSchemesExample/>
         <Button variant="primary" onClick={eventClick}>
           {data.value} {enumerar}
         </Button>
       </header>
     </div>
   );
+
+
 }
 
 export default App;
---
+++ src/pages/navegacion.js
@@ -0,0 +1,22 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function ColorSchemesExample() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Menú</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#features">Productos</Nav.Link>
+            {/* <Nav.Link href="#pricing">Pricing</Nav.Link> */}
+          </Nav>
+        </Container>
+      </Navbar>      
+    </>
+  );
+}
+
+export default ColorSchemesExample;
