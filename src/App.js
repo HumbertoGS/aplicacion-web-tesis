63a0c32e323d0c8a4cac7270897c3314ae63f9a5
@humberto_guiracocha <humberto.guiracocha@bitekso.com>
2022-12-18 17:04:53 -0500

    se agrega funcion click

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Button } from "react-bootstrap";
+import "./App.css";
+
+let data = { value: "Veces presionado: " };
 
 function App() {
+  const [enumerar, setEnumerar] = React.useState(0);
+
+  const eventClick = () => {
+    setEnumerar(enumerar + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant="primary" onClick={eventClick}>
+          {data.value} {enumerar}
+        </Button>
       </header>
     </div>
   );
