4817a5862c39812f12815b5f2cc6187e07685a0d
@humberto_guiracocha <humberto.guiracocha@bitekso.com>
2022-12-19 14:51:53 -0500

    Se agrega pantalla de registrar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./Pages/Content/Layout";
 import Inicio from "./Pages/Inicio";
+import Registrar from "./Pages/Registrar";
 import NoPage from "./Pages/NoPage";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route index element={<Inicio />} />
           {/* <Route path="catalogo" element={<Catalogo />} />
           <Route path="carrito" element={<Carrito />} /> */}
+          <Route path="registrar" element={<Registrar />} />
           <Route path="noPage" element={<NoPage />} />
           <Route path="*" element={<Navigate to="noPage" />} />
         </Route>
