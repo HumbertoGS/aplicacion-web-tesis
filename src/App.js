709e49e4097f0ef453ca7f160bf4f7afa4ff3e1e
@humberto_guiracocha <humberto.guiracocha@bitekso.com>
2022-12-30 17:42:50 -0500

    se agrega carrito a la rutas

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from "./Pages/Content/Layout";
 import Inicio from "./Pages/Inicio";
 import Registrar from "./Pages/Registrar";
 import RegistroVentas from "./Pages/RegistroVentas"
+import Carrito from "./Pages/Carrito";
 import NoPage from "./Pages/NoPage";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route index element={<Inicio />} />
           {/* <Route path="catalogo" element={<Catalogo />} />*/}
           <Route path="registro-ventas" element={<RegistroVentas />} /> 
+          <Route path="carrito" element={<Carrito />} />
           <Route path="registrar" element={<Registrar />} />
           <Route path="noPage" element={<NoPage />} />
           <Route path="*" element={<Navigate to="noPage" />} />
