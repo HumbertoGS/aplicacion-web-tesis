3518b398b81487ba0edd5ae4f7599f4ef9d48f97
@humberto_guiracocha <humberto.guiracocha@bitekso.com>
2023-01-15 01:28:15 -0500

    se actualiza plantilla

--- src/Pages/plantilla.js
+++ src/Pages/plantilla.js
@@ -4,10 +4,25 @@ import Breadcrumb from "react-bootstrap/Breadcrumb";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 
+import MensajeAlert from "./MensajeAlert";
+
 const Plantilla = () => {
+  const [mensaje, setMensaje] = useState("");
+  const [variant, setVariant] = useState("");
+
+  useEffect(() => {
+    if (variant) {
+      const interval = setTimeout(() => {
+        setVariant("");
+      }, 4000);
+      return () => clearInterval(interval);
+    }
+  }, [variant]);
+
   return (
     <>
       <Card body className="Card">
+        {variant ? <MensajeAlert variant={variant} mensaje={mensaje} /> : <></>}
         <Breadcrumb>
           <Breadcrumb.Item href="/">Inicio</Breadcrumb.Item>
           <Breadcrumb.Item active>Nombre_Seccion</Breadcrumb.Item>
@@ -15,8 +30,6 @@ const Plantilla = () => {
         {/* contenedores para el cuerpo */}
         <div></div>
         <Card></Card>
-        <Container></Container>
-        {/* incluso Grid */}
       </Card>
     </>
   );
