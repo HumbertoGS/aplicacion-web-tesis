02f7150faf5d5fd059824960c78157b1a5705fa6
FernandaHela <maria.herreral@ug.edu.ec>
2023-01-15 20:15:18 -0500

    detallito inv

--- src/Pages/Inventario/Inventario.js
+++ src/Pages/Inventario/Inventario.js
@@ -1,30 +1,43 @@
+import { useState } from "react";
+
 import Table from "react-bootstrap/Table";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
+import Dropdown from "react-bootstrap/Dropdown";
+import Button from "react-bootstrap/Button";
 
 import {
-    styleBtn,
-    styleBtnCancel,
-    styleBtnSave,
-    styleBtns,
-  } from "../designer/styleBtn";
+  styleBtn,
+  styleBtnCancel,
+  styleBtnSave,
+  styleBtns,
+} from "../designer/styleBtn";
 
 const productoTabla = [
-    {
-      id: 1,
-      producto: "Camisa",
-      cantidad: 2,
-      categoria: 3,
-      descripcion: "para entrega inmediata",
-    },
-    {
-        id: 2,
-        producto: "Shorts",
-        cantidad: 1,
-        categoria: 5,
-        descripcion: "para hombre",
-    }
+  {
+    id: 1,
+    producto: "Camisa",
+    cantidad: 2,
+    categoria: 3,
+    descripcion: "para entrega inmediata",
+  },
+  {
+    id: 2,
+    producto: "Shorts",
+    cantidad: 1,
+    categoria: 5,
+    descripcion: "para hombre",
+  },
 ];
+const Categorias = [
+  { id: 1, name: "Zapatos", estado: true },
+  { id: 2, name: "Vestidos", estado: true },
+  { id: 3, name: "Camisas", estado: true },
+  { id: 4, name: "Pantalones", estado: true },
+];
+
 function Inventario() {
+  //const [filtrarTabla, setFiltrarTabla] = useState("Filtro");
+  //const [filtro, setFiltro] = useState("Selecciona categoria");
   return (
     <div className="Card">
       <Breadcrumb>
@@ -33,6 +46,12 @@ function Inventario() {
       </Breadcrumb>
 
       <h5 className="text-center">Inventario de productos</h5>
+      <div>
+        <Dropdown.Toggle className="body" >
+          Filtro
+          {/*{filtrarTabla} */}
+        </Dropdown.Toggle>
+      </div>
       <Table>
         <thead className="theadTable">
           <tr>
@@ -44,19 +63,19 @@ function Inventario() {
           </tr>
         </thead>
         <tbody>
-        {productoTabla.map((item, index) => {
-          let style = item.estado ? styleBtnSave : styleBtnCancel;
-          return (
-            <tr key={index}>
-              <td>{item.id}</td>
-              <td>{item.producto}</td>
-              <td>{item.cantidad}</td>
-              <td>{item.categoria}</td>
-              <td>{item.descripcion}</td>
-            </tr>
-          );
-        })}
-      </tbody>
+          {productoTabla.map((item, index) => {
+            let style = item.estado ? styleBtnSave : styleBtnCancel;
+            return (
+              <tr key={index}>
+                <td>{item.id}</td>
+                <td>{item.producto}</td>
+                <td>{item.cantidad}</td>
+                <td>{item.categoria}</td>
+                <td>{item.descripcion}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </Table>
     </div>
   );
