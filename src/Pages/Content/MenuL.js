c67c46c487f23b7bbc9aa7bac179688dec53f288
@humberto_guiracocha <humberto.guiracocha@bitekso.com>
2023-01-20 00:33:52 -0500

    se valida menu lateral, para mostrar parte del menu solo si tiene acceso a ello, tambien se cambia nav.link por link

--- src/Pages/Content/MenuL.js
+++ src/Pages/Content/MenuL.js
@@ -1,85 +1,101 @@
+import { useState } from "react";
+
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Stack from "react-bootstrap/Stack";
 import Figure from "react-bootstrap/Figure";
 import { Link } from "react-router-dom";
+
 import "../designer/theme.css";
 
-import image from "../../assets/logo-negocio.jpeg";
+import { PostData } from "../../custom-hooks/accesoMenu";
 
-const MenuLateral = (
-  <Stack style={{ backgroundColor: "#e0c8db", width: "180px" }}>
-    <Navbar variant="white" style={{ backgroundColor: "#e0c8db" }}>
-      <Stack>
-        {/* <div>
+const MenuLateral = ({ user }) => {
+  return (
+    <Stack style={{ backgroundColor: "#e0c8db", width: "180px" }}>
+      <Navbar variant="white" style={{ backgroundColor: "#e0c8db" }}>
+        <Stack>
+          {/* <div>
           <img src={image} />
         </div> */}
-        <Container>
-          <Figure>
-            <Figure.Image
-              width={150}
-              // height={300}
-              alt="Imagen del negocio"
-              src={"logo512.png"}
-            />
-          </Figure>
-        </Container>
-        <Container>
-          <Nav className="me-auto">
-            <Nav.Link href="/">Inicio</Nav.Link>
-          </Nav>
-        </Container>
-        <Container>
-          <Nav className="me-auto">
-            <Nav.Link href="Catalogo">Catalogo</Nav.Link>
-          </Nav>
-        </Container>
-        <Container>
-          <Nav className="me-auto">
-            <Nav.Link href="Carrito">Carrito</Nav.Link>
-          </Nav>
-        </Container>
-        <Container>
-          <Nav className="me-auto">
-            <Nav.Link href="Registrar-Productos">
-              Registro de Productos
-            </Nav.Link>
-          </Nav>
-        </Container>
-        <Container>
-          <Nav className="me-auto">
-            <Nav.Link href="Registro-Ventas">Registro de Ventas</Nav.Link>
-          </Nav>
-        </Container>
-        <Container>
-          <Nav className="me-auto">
-            <Nav.Link href="Status-Pedido">Status del Pedido</Nav.Link>
-          </Nav>
-        </Container>
-        <Container>
-          <Nav className="me-auto">
-            <Nav.Link href="Inventario">Inventario</Nav.Link>
-          </Nav>
-        </Container>
-      </Stack>
-    </Navbar>
-
-    {/* <Link to="/">
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand>Inicio</Navbar.Brand>
-        </Container>
+          <Container>
+            <Figure>
+              <Figure.Image
+                width={150}
+                // height={300}
+                alt="Imagen del negocio"
+                src={"logo512.png"}
+              />
+            </Figure>
+          </Container>
+          <Link to="Inicio">
+            <Container className="py-2 text-start nav-element" id="navL">
+              <label className="mx-2 nav-element">Inicio</label>
+            </Container>
+          </Link>
+          <Link to="Catalogo">
+            <Container className="py-2 text-start nav-element" id="navL">
+              <label className="mx-2 nav-element">Catalogo</label>
+            </Container>
+          </Link>
+          <Link to="Carrito">
+            <Container className="py-2 text-start nav-element" id="navL">
+              <label className="mx-2 nav-element">Carrito</label>
+            </Container>
+          </Link>
+          <Link to="Status-Pedido">
+            <Container className="py-2 text-start nav-element" id="navL">
+              <label className="mx-2 nav-element">Status del Pedido</label>
+            </Container>
+          </Link>
+          {user ? (
+            <div>
+              <Link to="Registrar-Productos">
+                <Container className="py-2 text-start nav-element" id="navL">
+                  <label className="mx-2 nav-element">
+                    Registro de Productos
+                  </label>
+                </Container>
+              </Link>
+              <Link to="Registro-Ventas">
+                <Container className="py-2 text-start nav-element" id="navL">
+                  <label className="mx-2 nav-element">Registro de Ventas</label>
+                </Container>
+              </Link>
+              <Link to="Inventario">
+                <Container className="py-2 text-start nav-element" id="navL">
+                  <label className="mx-2 nav-element">Inventario</label>
+                </Container>
+              </Link>
+              {/* <Container>
+                <Nav className="me-auto">
+                  <Nav.Link href="Registrar-Productos">
+                    Registro de Productos
+                  </Nav.Link>
+                </Nav>
+              </Container> */}
+            </div>
+          ) : (
+            <></>
+          )}
+        </Stack>
       </Navbar>
-    </Link> */}
-  </Stack>
-);
+    </Stack>
+  );
+};
 
 function MenuL() {
+  const [user, setUser] = useState(false);
+
+  PostData((datos) => {
+    setUser(datos);
+  });
+
   return (
     <>
       <div className="App-menu">
-        {MenuLateral}
+        <MenuLateral user={user} />
         <div className="footer-menu">
           <span>
             Developed by
