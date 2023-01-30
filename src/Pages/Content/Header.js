dac5ff01031e83dbed16201826be809a13c76e9c
@humberto_guiracocha <humberto.guiracocha@bitekso.com>
2023-01-30 00:23:41 -0500

    se añade iconos

--- src/Pages/Content/Header.js
+++ src/Pages/Content/Header.js
@@ -8,7 +8,10 @@ import Card from "react-bootstrap/Card";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Figure from "react-bootstrap/Figure";
 import Button from "react-bootstrap/esm/Button";
+
 import { Link } from "react-router-dom";
+import { AiOutlineUser } from "react-icons/ai";
+import { RiLogoutBoxRLine, RiLogoutBoxLine } from "react-icons/ri";
 
 import "../designer/theme.css";
 
@@ -19,7 +22,7 @@ const Header = () => {
   const logout = () => {
     // localStorage.removeItem("user");
     secureLocalStorage.removeItem("user");
-    window.location.href = process.env.REACT_APP_BASENAME + "Inicio";
+    window.location.href = process.env.REACT_APP_BASENAME + "Catalogo";
   };
 
   return (
@@ -41,80 +44,96 @@ const Header = () => {
 
           {user ? (
             <Navbar.Collapse className="justify-content-end">
-              <Navbar.Text>
+              <Navbar.Text className="px-5">
                 Conectado como: <Link to="Inicio">{user?.nombre}</Link>
               </Navbar.Text>
 
-              <NavDropdown className="px-5" title="Perfil">
-                <Link to="Inicio">
-                  <Container className="py-1 text-start opc-element" id="navL2">
-                    <label className="mx-1" id="label2">
-                      Mis datos
-                    </label>
-                  </Container>
-                </Link>
-
-                {user ? (
-                  user.permisos == 1 ? (
-                    <div>
-                      <Link to="Inventario">
-                        <Container
-                          className="py-1 text-start opc-element"
-                          id="navL2"
-                        >
-                          <label className="mx-1" id="label2">
-                            Inventario
-                          </label>
-                        </Container>
-                      </Link>
-                    </div>
-                  ) : (
-                    <div>
-                      <Link to="Carrito">
-                        <Container
-                          className="py-1 text-start opc-element"
-                          id="navL2"
-                        >
-                          <label className="mx-1" id="label2">
-                            Carrito
-                          </label>
-                        </Container>
-                      </Link>
-
-                      <Link to="Status-Pedido">
-                        <Container
-                          className="py-1 text-start opc-element"
-                          id="navL2"
-                        >
-                          <label className="mx-1" id="label2">
-                            Estado de Pedido
-                          </label>
-                        </Container>
-                      </Link>
-                    </div>
-                  )
-                ) : (
-                  <></>
-                )}
-
-                <NavDropdown.Divider />
-
-                <Container
-                  className="py-1 text-start opc-element"
-                  id="navL2"
-                  onClick={logout}
+              <div className="d-flex align-items-center px-5">
+                <AiOutlineUser />
+                <NavDropdown
+                  style={{ paddingRight: "35px", paddingLeft: "5px" }}
+                  title="Perfil"
                 >
-                  <label className="mx-1" id="label2">
-                    Cerrar Sesion
-                  </label>
-                </Container>
-              </NavDropdown>
+                  <Link to="Inicio">
+                    <Container
+                      className="py-1 text-start opc-element"
+                      id="navL2"
+                    >
+                      <label className="mx-1" id="label2">
+                        Mis datos
+                      </label>
+                    </Container>
+                  </Link>
+
+                  {user ? (
+                    user.permisos == 1 ? (
+                      <div>
+                        <Link to="Inventario">
+                          <Container
+                            className="py-1 text-start opc-element"
+                            id="navL2"
+                          >
+                            <label className="mx-1" id="label2">
+                              Inventario
+                            </label>
+                          </Container>
+                        </Link>
+                      </div>
+                    ) : (
+                      <div>
+                        <Link to="Carrito">
+                          <Container
+                            className="py-1 text-start opc-element"
+                            id="navL2"
+                          >
+                            <label className="mx-1" id="label2">
+                              Carrito
+                            </label>
+                          </Container>
+                        </Link>
+
+                        <Link to="Status-Pedido">
+                          <Container
+                            className="py-1 text-start opc-element"
+                            id="navL2"
+                          >
+                            <label className="mx-1" id="label2">
+                              Estado de Pedido
+                            </label>
+                          </Container>
+                        </Link>
+                      </div>
+                    )
+                  ) : (
+                    <></>
+                  )}
+
+                  <NavDropdown.Divider />
+
+                  <Container
+                    className="py-1 text-start opc-element d-flex align-items-center"
+                    id="navL2"
+                    onClick={logout}
+                  >
+                    <label className="mx-1" id="label2">
+                      Cerrar Sesion
+                    </label>
+                    <RiLogoutBoxLine />
+                  </Container>
+                </NavDropdown>
+              </div>
             </Navbar.Collapse>
           ) : (
-            <Navbar.Collapse className="justify-content-end">
-              <Navbar.Text>
-                <Link to="Ingresar">Ingresar</Link>
-              </Navbar.Text>
+            <Navbar.Collapse className="px-5 justify-content-end">
+              <div
+                className="d-flex align-items-center"
+                style={{ paddingRight: "35px" }}
+              >
+                <RiLogoutBoxRLine />
+                <Navbar.Text className="px-1">
+                  <Link to="Ingresar">Ingresar</Link>
+                </Navbar.Text>
+              </div>
             </Navbar.Collapse>
           )}
         </Container>
