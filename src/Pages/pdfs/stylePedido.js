d1e5060f73d8317a7ec9c359a730351146330c70
@humberto_guiracocha <humberto.guiracocha@bitekso.com>
2023-02-11 15:56:07 -0500

    se agrega stylos para pdf pedido

---
+++ src/Pages/pdfs/stylePedido.js
@@ -0,0 +1,82 @@
+const stylesPedido = {
+  pdf: {
+    paddingHorizontal: "30px",
+    paddingTop: "20px",
+  },
+  encabezado: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginRight: "20px",
+  },
+  titulo: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    paddingLeft: "30px",
+    color: "#da6b9e",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  table: {
+    width: "100%",
+    paddingRight: "60px",
+    paddingLeft: "30px",
+    paddingVertical: "20px",
+    border: "1px solid #EEE",
+  },
+  rowTable: {
+    display: "flex",
+    flexDirection: "row",
+    borderTop: "1px solid #EEE",
+    paddingTop: 8,
+    paddingBottom: 8,
+  },
+  rowTable2: {
+    borderTop: "none",
+    fontWeight: "bold",
+  },
+  tableSpace: {
+    marginBottom: "30px",
+    borderBottom: "1px solid #EEE",
+  },
+  text: {
+    fontSize: "14px",
+    marginTop: "15px",
+    marginBottom: "20px",
+  },
+  textWs: { color: "red" },
+  cellProperty: {
+    cellWidth: {
+      Row10: {
+        width: "10%",
+      },
+      Row20: {
+        width: "20%",
+      },
+      Row30: {
+        width: "30%",
+      },
+      Row40: {
+        width: "40%",
+      },
+      Row50: {
+        width: "50%",
+      },
+      Row70: {
+        width: "70%",
+      },
+    },
+    fontSize: {
+      header: { fontSize: "12px" },
+      body: { fontSize: "11px" },
+    },
+    fontWeight: "bold",
+  },
+};
+
+export { stylesPedido };
