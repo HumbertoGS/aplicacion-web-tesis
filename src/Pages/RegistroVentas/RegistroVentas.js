c1c375d71e488ffdd8475d19d8ea03ea007f6073
@humberto_guiracocha <humberto.guiracocha@bitekso.com>
2023-02-14 17:08:48 -0500

    se llama a componentes para tabla

--- src/Pages/RegistroVentas/RegistroVentas.js
+++ src/Pages/RegistroVentas/RegistroVentas.js
@@ -25,38 +25,6 @@ import ModalVentasDetalles from "./ModalVentasDetalles";
 import ModalBtns from "./ModalBtns";
 import ClienteDetalles from "./Modal/ClienteDetalles";
 
-const columns = [
-  [
-    { name: "", colSpan: 1, style: { borderRight: "1px solid #c8c9ca" } },
-    {
-      name: "Cliente",
-      colSpan: 2,
-      style: { borderRight: "1px solid #c8c9ca" },
-    },
-    { name: "Pedido", colSpan: 3, style: { borderRight: "1px solid #c8c9ca" } },
-    { name: "", colSpan: 2 },
-  ],
-  [
-    {
-      name: "N° Pedido",
-      style: { width: "9%", borderRight: "1px solid #c8c9ca" },
-    },
-    { name: "Cliente", style: { width: "200px" } },
-    {
-      name: "Detalles",
-      style: { width: "10%", borderRight: "1px solid #c8c9ca" },
-    },
-    { name: "N° Productos", style: { width: "11%" } },
-    { name: "Total", style: { width: "9%" } },
-    {
-      name: "Detalles",
-      style: { width: "10%", borderRight: "1px solid #c8c9ca" },
-    },
-    { name: "N° transferencia", style: { width: "300px" } },
-    { name: "Acciones", style: { width: "22%" } },
-  ],
-];
-
 const datosP = [
   {
     id: 1,
@@ -185,103 +153,6 @@ const RegistroVentas = () => {
   return (
     <>
       <Card body className="Card" style={{ minHeight: "87vh" }}>
-        {/* <div className="my-4 mx-3">
-          <h5 className="text-center">Pedidos en Espera</h5>
-          <hr />
-          <div style={{ overflowY: "auto" }}>
-            <Table striped hover>
-              <thead className="theadTable">
-                {columns.map((row, i) => (
-                  <tr key={i}>
-                    {row.map((cell, j) => (
-                      <th
-                        key={j}
-                        colSpan={cell.colSpan || 1}
-                        style={cell.style || null}
-                      >
-                        {cell.name}
-                      </th>
-                    ))}
-                  </tr>
-                ))}
-              </thead>
-              <tbody>
-                {datos.map((item, index) => {
-                  return !item.validado && item.estado ? (
-                    <tr key={item.id}>
-                      <td>{item.num_pedido}</td>
-                      <td>{item.num_identificacion}</td>
-                      <td>
-                        <Button
-                          style={styleBtn}
-                          onClick={() => {
-                            setDatosDetalles(item);
-                            setModal(true);
-                          }}
-                          variant="outline-secondary"
-                        >
-                          <FaEye />
-                        </Button>
-                      </td>
-                      <td>{item.num_pedidos}</td>
-                      <td>$15.50</td>
-                      <td>
-                        <Button
-                          style={styleBtn}
-                          onClick={() => {
-                            setDatosDetalles(item);
-                            setModal(true);
-                          }}
-                          variant="outline-secondary"
-                        >
-                          <FaEye />
-                        </Button>
-                      </td>
-                      <td>
-                        <InputGroup>
-                          <Form.Control
-                            style={{ marginLeft: "10px" }}
-                            readOnly={item.validado}
-                            disabled={item.validado}
-                            value={item.transferencia}
-                            onChange={(event) =>
-                              handleInputChange(event, index)
-                            }
-                          />
-                        </InputGroup>
-                      </td>
-                      <td>
-                        <div style={styleBtns}>
-                          <Form.Select
-                            className="mx-2"
-                            value={item.status}
-                            onChange={(event) =>
-                              handleSelectChange(event, index)
-                            }
-                          >
-                            <option value="">Seleccione una opcion</option>
-                            <option value="1">Pendiente</option>
-                            <option value="2">Pagado</option>
-                            <option value="0">Cancelado</option>
-                          </Form.Select>
-                          <Button
-                            disabled={item.transferencia === ""}
-                            style={{ ...styleBtn, ...styleBtnSave }}
-                            onClick={() => handleButtonClick(index)}
-                          >
-                            <BsCheck2 />
-                          </Button>
-                        </div>
-                      </td>
-                    </tr>
-                  ) : (
-                    <></>
-                  );
-                })}
-              </tbody>
-            </Table>
-          </div>
-        </div> */}
         <TablaPedidos
           Titulo="Pedidos en espera"
           data={datos}
@@ -295,21 +166,22 @@ const RegistroVentas = () => {
           }}
         />
       </Card>
+
       {modalPedido ? (
         <ModalVentasDetalles
           detalles={datosDetalles}
           show={modalPedido}
           onHide={() => setModalPedido(false)}
         />
-      ) : modalCliente ? (
+      ) : null}
+
+      {modalCliente ? (
         <ClienteDetalles
           data={idCliente}
           show={modalCliente}
           onHide={() => setModalCliente(false)}
         />
-      ) : (
-        <></>
-      )}
+      ) : null}
     </>
   );
 };
