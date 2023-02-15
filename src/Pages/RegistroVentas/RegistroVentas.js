e8d709794c467a6137e0e1dc52f968939edb8b49
@humberto_guiracocha <humberto.guiracocha@bitekso.com>
2023-02-15 01:15:47 -0500

    se elimina codigo no utilizado, se refactor

--- src/Pages/RegistroVentas/RegistroVentas.js
+++ src/Pages/RegistroVentas/RegistroVentas.js
@@ -1,44 +1,23 @@
-import { useEffect, useState } from "react";
-
-import Table from "react-bootstrap/Table";
-import Breadcrumb from "react-bootstrap/Breadcrumb";
+import { useState } from "react";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
 
 import TablaPedidos from "./TablaPedidos";
-
-import { FaEye } from "react-icons/fa";
-import { IoMdClose } from "react-icons/io";
-import { BsCheck2 } from "react-icons/bs";
-
-import {
-  styleBtn,
-  styleBtnCancel,
-  styleBtnSave,
-  styleBtns,
-} from "../designer/styleBtn";
-
 import PedidoDetalles from "./Modal/PedidoDetalles";
-import ModalBtns from "./ModalBtns";
 import ClienteDetalles from "./Modal/ClienteDetalles";
+
 import { PostData, ReloadData } from "../../custom-hooks/useFetch";
 
 const urlPedidos = `${process.env.REACT_APP_API_CORE_URL}pedido/`;
 
 const RegistroVentas = () => {
+  const [datos, setDatos] = useState(null);
   const [cargar, setCargar] = useState(true);
+
   const [numeroPedido, setNumeroPedido] = useState([]);
   const [idCliente, setIdCliente] = useState(null);
-  const [valido, setValido] = useState(false);
-  const [numero, setNumero] = useState("");
 
   const [modalPedido, setModalPedido] = useState(false);
   const [modalCliente, setModalCliente] = useState(false);
-
-  const [datos, setDatos] = useState(null);
 
   ReloadData(urlPedidos, cargar, (x) => {
     setDatos(x?.datos);
@@ -64,21 +43,21 @@ const RegistroVentas = () => {
         )}
       </Card>
 
-      {modalPedido ? (
+      {modalPedido && (
         <PedidoDetalles
           data={numeroPedido}
           show={modalPedido}
           onHide={() => setModalPedido(false)}
         />
-      ) : null}
+      )}
 
-      {modalCliente ? (
+      {modalCliente && (
         <ClienteDetalles
           data={idCliente}
           show={modalCliente}
           onHide={() => setModalCliente(false)}
         />
-      ) : null}
+      )}
     </>
   );
 };
