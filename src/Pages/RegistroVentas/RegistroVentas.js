6e6ce159dbd737940beb847cac72a4255e400b7c
@humberto_guiracocha <humberto.guiracocha@bitekso.com>
2023-02-16 18:31:14 -0500

    se agrega filtro de busqueda tipo accordion

--- src/Pages/RegistroVentas/RegistroVentas.js
+++ src/Pages/RegistroVentas/RegistroVentas.js
@@ -1,13 +1,20 @@
 import { useState } from "react";
-import Card from "react-bootstrap/Card";
+import { Accordion, Button, Card, Form } from "react-bootstrap";
 
 import TablaPedidos from "./TablaPedidos";
 import PedidoDetalles from "./Modal/PedidoDetalles";
 import ClienteDetalles from "./Modal/ClienteDetalles";
 
 import { ReloadData } from "../../custom-hooks/useFetch";
+import { BtnGuardar } from "../components/BtnAccion";
 
-const urlPedidos = `${process.env.REACT_APP_API_CORE_URL}pedido/`;
+const urlPedidos = `${process.env.REACT_APP_API_CORE_URL}pedido`;
+
+const busqueda = {
+  status: 1,
+  num_pedido: "",
+  num_ident: "",
+};
 
 const RegistroVentas = () => {
   const [datos, setDatos] = useState(null);
@@ -15,6 +22,7 @@ const RegistroVentas = () => {
 
   const [numeroPedido, setNumeroPedido] = useState([]);
   const [idCliente, setIdCliente] = useState(null);
+  const [titulo, setTitulo] = useState("en Espera");
 
   const [modalPedido, setModalPedido] = useState(false);
   const [modalCliente, setModalCliente] = useState(false);
@@ -24,12 +32,134 @@ const RegistroVentas = () => {
     setCargar(false);
   });
 
+  const [open, setOpen] = useState(true);
+  const [datosBuscar, setDatosBuscar] = useState(null);
+
+  const onlyNumber = (event) => {
+    const keyCode = event.keyCode || event.which;
+    const keyValue = String.fromCharCode(keyCode);
+    if (!/^\d+$/.test(keyValue) && ![8, 46, 37, 39].includes(keyCode)) {
+      event.preventDefault();
+    }
+  };
+
+  const BusquedaAvz = () => {
+    return (
+      <>
+        <div className="d-flex py-2" style={{ justifyContent: "space-around" }}>
+          <Form.Group className="d-flex flex-row align-items-center">
+            <Form.Label className="fw-bold w-50">Estado del pedido</Form.Label>
+            <Form.Select
+              className="mx-3 w-75"
+              defaultValue={datosBuscar?.status}
+              onChange={(event) => {
+                busqueda.status = event.target.value;
+              }}
+            >
+              <option value={1}>Pendiente</option>
+              <option value={2}>Pagado</option>
+              <option value={3}>Cancelado</option>
+            </Form.Select>
+          </Form.Group>
+
+          <Form.Group className="d-flex flex-row align-items-center">
+            <Form.Label className="fw-bold w-50">Número del pedido</Form.Label>
+            <Form.Control
+              className="mx-3 w-75"
+              type="text"
+              maxLength={5}
+              defaultValue={datosBuscar?.num_pedido}
+              onKeyDown={(e) => {
+                onlyNumber(e);
+              }}
+              onChange={(event) => {
+                busqueda.num_pedido = event.target.value;
+              }}
+            />
+          </Form.Group>
+
+          <Form.Group className="d-flex flex-row align-items-center">
+            <Form.Label className="fw-bold w-50">
+              Identificación del cliente
+            </Form.Label>
+            <Form.Control
+              className="mx-3 w-75"
+              type="text"
+              maxLength={10}
+              defaultValue={datosBuscar?.num_ident}
+              onKeyDown={(e) => {
+                onlyNumber(e);
+              }}
+              onChange={(event) => {
+                busqueda.num_ident = event.target.value;
+              }}
+            />
+          </Form.Group>
+        </div>
+        <div className="d-flex justify-content-center">
+          <BtnGuardar
+            datos={busqueda}
+            url={urlPedidos + "/buscar"}
+            handleRespond={(x) => {
+              setDatos(x);
+              setTitulo(
+                busqueda.status === "1"
+                  ? "en Espera"
+                  : busqueda.status === "2"
+                  ? "Pagados"
+                  : "Cancelados"
+              );
+              setDatosBuscar(busqueda);
+              setOpen(!open);
+            }}
+            mensajeResp="Búsqueda Realizada"
+          />
+          <Button
+            className="mx-2"
+            variant="outline-secondary"
+            disabled={!datosBuscar}
+            onClick={() => {
+              busqueda.num_pedido = "";
+              busqueda.num_ident = "";
+              setDatosBuscar({
+                status: busqueda.status,
+                num_pedido: "",
+                num_ident: "",
+              });
+
+              // setTitulo("en Espera");
+              // setDatosBuscar(null);
+
+              // console.log(busqueda)
+            }}
+          >
+            Limpiar
+          </Button>
+        </div>
+      </>
+    );
+  };
+
   return (
     <>
       <Card body className="Card" style={{ minHeight: "70vh" }}>
+        <Accordion>
+          <Accordion.Item eventKey={open}>
+            <Accordion.Header>
+              <Form.Label className="fw-bold w-50 my-0">
+                Filtro de Búsqueda
+              </Form.Label>
+              <hr />
+            </Accordion.Header>
+            <Accordion.Body>
+              <BusquedaAvz />
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
         {datos && (
           <TablaPedidos
-            Titulo="Pedidos en espera"
+            Titulo={"Pedidos " + titulo}
+            filtro={datosBuscar}
             data={datos}
             reload={() => {
               const interval = setTimeout(() => {
@@ -52,6 +182,7 @@ const RegistroVentas = () => {
       {modalPedido && (
         <PedidoDetalles
           data={numeroPedido}
+          filtro={datosBuscar}
           show={modalPedido}
           onHide={() => setModalPedido(false)}
         />
