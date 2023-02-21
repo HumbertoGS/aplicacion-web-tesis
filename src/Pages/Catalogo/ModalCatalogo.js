15c377f7c364bb79184a83ca631a8e3b18c233b4
@humberto_guiracocha <humberto.guiracocha@bitekso.com>
2023-02-21 00:54:52 -0500

    se hace uso de nuevo componente

--- src/Pages/Catalogo/ModalCatalogo.js
+++ src/Pages/Catalogo/ModalCatalogo.js
@@ -5,9 +5,13 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 import InputGroup from "react-bootstrap/InputGroup";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+
 import { FaCartPlus } from "react-icons/fa";
 
 import "../designer/theme.css";
+import ZoomImage from "./ZoomImage";
 
 const ModalCatalogo = (props) => {
   const producto = props.producto;
@@ -21,18 +25,12 @@ const ModalCatalogo = (props) => {
     };
     setCantidad(0);
     props.datosCarrito(Carrito);
-    props.onHide()
+    props.onHide();
   };
 
   return (
     <>
-      <Modal
-        show={props.show}
-        onHide={props.onHide}
-        // aria-labelledby="contained-modal-title-vcenter"
-        centered
-        size="lg"
-      >
+      <Modal show={props.show} onHide={props.onHide} centered size="lg">
         <div>
           <Modal.Header closeButton>
             <Modal.Title id="contained-modal-title-vcenter">
@@ -40,86 +38,101 @@ const ModalCatalogo = (props) => {
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <Card className="Card" style={{ flexDirection: "row" }}>
-              <Card.Img
-                variant="top"
-                style={{
-                  width: "500px",
-                  height: "400px",
-                }}
-                src={producto.imagen}
-              />
-              <Card.Body>
-                <div className="px-2">
-                  <Card.Title>
-                    {producto.nombre ?? producto.nombre_categoria}
-                    <hr />
-                  </Card.Title>
-                  <div>
-                    <InputGroup className="mb-3">
-                      <InputGroup.Text className="w-50">Precio</InputGroup.Text>
-                      <Form.Control
-                        disabled
-                        defaultValue={"$" + producto.precio}
-                      />
-                    </InputGroup>
-
-                    <InputGroup className="mb-3">
-                      <InputGroup.Text className="w-50">Talla</InputGroup.Text>
-                      <Form.Control disabled defaultValue={producto.talla} />
-                    </InputGroup>
-
-                    <InputGroup className="mb-3">
-                      <InputGroup.Text className="w-50">Disponible</InputGroup.Text>
-                      <Form.Control disabled defaultValue={producto.cantidad} />
-                    </InputGroup>
-
-                    <InputGroup className="mb-3">
-                      <InputGroup.Text className="w-50">
-                        Cantidad
-                      </InputGroup.Text>
-                      <Form.Control
-                        onChange={(event) => {
-                          setCantidad(event.target.value);
-                        }}
-                        placeholder={cantidad}
-                        value={cantidad == 0 ? "" : cantidad}
-                      />
-                    </InputGroup>
-                  </div>
-                  <Button
-                    variant="dark"
-                    disabled={cantidad == 0}
+            <Card className="Card flex-row">
+              <Row>
+                <Col md={7}>
+                  <div
+                    className="CardImagen px-2"
                     style={{
-                      width: "100%",
+                      width: "450px",
+                      height: "400px",
                     }}
-                    onClick={addCar}
                   >
-                    <FaCartPlus />
-                  </Button>
-                  <div className="py-3">
-                    <InputGroup className="mb-3">
-                      <InputGroup.Text
-                        className="w-100"
-                        style={{ borderRadius: "0px" }}
-                      >
-                        Descripcion
-                      </InputGroup.Text>
-                      <Form.Control
-                        style={{ border: "0px" }}
-                        disabled
-                        className="bg-white text-center pt-3"
-                        defaultValue={producto.descripcion}
-                      />
-                    </InputGroup>
+                    <ZoomImage src={producto.imagen} width={450} height={400} />
                   </div>
-                </div>
-              </Card.Body>
+                </Col>
+                <Col md={5}>
+                  <Card.Body className="bg-white">
+                    <div className="px-2">
+                      <Card.Title>
+                        {producto?.nombre}
+                        <hr />
+                      </Card.Title>
+                      <div>
+                        <InputGroup className="mb-3">
+                          <InputGroup.Text className="w-50">
+                            Precio
+                          </InputGroup.Text>
+                          <Form.Control
+                            disabled
+                            defaultValue={"$" + producto.precio}
+                          />
+                        </InputGroup>
+
+                        <InputGroup className="mb-3">
+                          <InputGroup.Text className="w-50">
+                            Talla
+                          </InputGroup.Text>
+                          <Form.Control
+                            disabled
+                            defaultValue={producto.talla}
+                          />
+                        </InputGroup>
+
+                        <InputGroup className="mb-3">
+                          <InputGroup.Text className="w-50">
+                            Disponible
+                          </InputGroup.Text>
+                          <Form.Control
+                            disabled
+                            defaultValue={producto.cantidad}
+                          />
+                        </InputGroup>
+
+                        <InputGroup className="mb-3">
+                          <InputGroup.Text className="w-50">
+                            Cantidad
+                          </InputGroup.Text>
+                          <Form.Control
+                            autoComplete="off"
+                            onChange={(event) => {
+                              setCantidad(event.target.value);
+                            }}
+                            placeholder={cantidad}
+                            value={cantidad === 0 ? "" : cantidad}
+                          />
+                        </InputGroup>
+                      </div>
+                      <Button
+                        variant="dark"
+                        disabled={cantidad === 0}
+                        className="w-100"
+                        onClick={addCar}
+                      >
+                        <FaCartPlus />
+                      </Button>
+                      <div className="py-3">
+                        <InputGroup className="mb-3">
+                          <InputGroup.Text
+                            className="w-100"
+                            style={{ borderRadius: "0px" }}
+                          >
+                            Descripcion
+                          </InputGroup.Text>
+                          <Form.Control
+                            style={{ border: "0px" }}
+                            disabled
+                            className="bg-white text-center pt-3"
+                            defaultValue={producto.descripcion}
+                          />
+                        </InputGroup>
+                      </div>
+                    </div>
+                  </Card.Body>
+                </Col>
+              </Row>
             </Card>
           </Modal.Body>
-          {/* <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer> */}
         </div>
       </Modal>
     </>
