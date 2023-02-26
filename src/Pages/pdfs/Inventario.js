6b7c0976fc7b534dcfe6b2448154a1f7faffadf0
@humberto_guiracocha <humberto.guiracocha@bitekso.com>
2023-02-26 03:54:14 -0500

    se agregan tablas para reporte

--- src/Pages/pdfs/Inventario.js
+++ src/Pages/pdfs/Inventario.js
@@ -24,8 +24,10 @@ const TablaDetalles = ({ datos }) => {
       <View style={styleHeader}>
         <Text style={style({ w: 2, f: "header" })}>Codigo</Text>
         <Text style={style({ w: 6, f: "header" })}>Producto</Text>
-        <Text style={style({ w: 3, f: "header" })}>Precio</Text>
-        <Text style={style({ w: 3, f: "header" })}>Categoria</Text>
+        <Text style={style({ w: 2, f: "header" })}>Precio</Text>
+        <Text style={style({ w: 2, f: "header" })}>Disponible</Text>
+        <Text style={style({ w: 2, f: "header" })}>Vendido</Text>
+        <Text style={style({ w: 3, f: "header" })}>Fecha Registro</Text>
         <Text style={style({ w: 2, f: "header" })}>Stock</Text>
       </View>
 
@@ -33,8 +35,18 @@ const TablaDetalles = ({ datos }) => {
         <View key={i} style={stylesPdf.rowTable} wrap={false}>
           <Text style={style({ w: 2, f: "body" })}>{row.codigo}</Text>
           <Text style={style({ w: 6, f: "body" })}>{row.nombre}</Text>
-          <Text style={style({ w: 3, f: "body" })}>${row.precio}</Text>
-          <Text style={style({ w: 3, f: "body" })}>{row.nombre_categoria}</Text>
+          <Text style={style({ w: 2, f: "body" })}>${row.precio}</Text>
+          <Text
+            style={[...style({ w: 2, f: "body" }), { paddingLeft: "20px" }]}
+          >
+            {row.cantidad}
+          </Text>
+          <Text
+            style={[...style({ w: 2, f: "body" }), { paddingLeft: "20px" }]}
+          >
+            {row.vendido}
+          </Text>
+          <Text style={style({ w: 3, f: "body" })}>{row.fecha_registro}</Text>
           <Text
             style={[
               style({ w: 2, f: "body" }),
@@ -49,12 +61,70 @@ const TablaDetalles = ({ datos }) => {
   );
 };
 
-const InventarioPdf = ({ datos, fecha }) => {
+const TablaOtrosDetalles = ({ datosPDF }) => {
+  const { datosCategoria, datosProducto } = datosPDF;
+  const styleHeader = [stylesPdf.rowTable, stylesPdf.rowTable2];
+
+  const widthCell = stylesPdf.cellProperty.cellWidth;
+  const fontSize = stylesPdf.cellProperty.fontSize;
+
+  const style = ({ w, f }) => [widthCell[`Row${w}0`], fontSize[f]];
+
+  return (
+    <View style={{ display: "flex", flexDirection: "row", width: "100%" }}>
+      <View style={{ width: "47%" }}>
+        <Text style={stylesPdf.text}>Categoria más Vendida</Text>
+        <View style={stylesPdf.table}>
+          <View style={styleHeader}>
+            <Text style={style({ w: 5, f: "header" })}>Categoria</Text>
+            <Text style={style({ w: 2, f: "header" })}>Cantidad</Text>
+          </View>
+
+          {datosCategoria.map((row, i) => (
+            <View key={i} style={stylesPdf.rowTable} wrap={false}>
+              <Text style={style({ w: 5, f: "body" })}>{row.subject}</Text>
+              <Text
+                style={[...style({ w: 2, f: "body" }), { paddingLeft: "20px" }]}
+              >
+                {row.conteo}
+              </Text>
+            </View>
+          ))}
+        </View>
+      </View>
+      <View style={{ width: "6%" }}></View>
+      <View style={{ width: "47%" }}>
+        <Text style={stylesPdf.text}>Producto más Vendido</Text>
+        <View style={stylesPdf.table}>
+          <View style={styleHeader}>
+            <Text style={style({ w: 7, f: "header" })}>Producto</Text>
+            <Text style={style({ w: 2, f: "header" })}>Cantidad</Text>
+          </View>
+
+          {datosProducto.map((row, i) => (
+            <View key={i} style={stylesPdf.rowTable} wrap={false}>
+              <Text style={style({ w: 7, f: "body" })}>{row.name}</Text>
+              <Text
+                style={[...style({ w: 2, f: "body" }), { paddingLeft: "20px" }]}
+              >
+                {row.conteo}
+              </Text>
+            </View>
+          ))}
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const InventarioPdf = ({ datos, fecha, datosPDF }) => {
   return (
     <Document>
-      <Page size="A4" style={stylesPdf.pdf}>
+      <Page size="A4" style={stylesPdf.pdf} orientation="landscape">
         <View style={stylesPdf.encabezado}>
-          <Text style={stylesPdf.titulo}>Novedades D'Myla & Ney</Text>
+          <Text style={{ ...stylesPdf.titulo, paddingRight: "200px" }}>
+            Novedades D'Myla & Ney
+          </Text>
           <Image src={"logo512.png"} style={stylesPdf.logo} />
         </View>
 
@@ -68,6 +138,10 @@ const InventarioPdf = ({ datos, fecha }) => {
         <View style={stylesPdf.tableSpace}></View>
 
         <TablaDetalles datos={datos} />
+
+        <View style={stylesPdf.tableSpace}></View>
+        
+        <TablaOtrosDetalles datosPDF={datosPDF} />
       </Page>
     </Document>
   );
