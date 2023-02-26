902dc2a9dff8b67d6fd077b07018151d17797548
@humberto_guiracocha <humberto.guiracocha@bitekso.com>
2023-02-26 01:59:49 -0500

    se agrega graficas

--- src/Pages/Perfil/Graficos.js
+++ src/Pages/Perfil/Graficos.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Col, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Col, Row } from "react-bootstrap";
 import {
   ResponsiveContainer,
   Tooltip,
@@ -14,74 +14,56 @@ import {
   PieChart,
   Pie,
   Cell,
+  //Grafica radar
+  Radar,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  //Grafica linear
+  LineChart,
+  Line,
 } from "recharts";
+import { ReloadData } from "../../custom-hooks/useFetch";
 
-const GraficoBarra = () => {
-  const data = [
-    {
-      name: "luis",
-      age: 12,
-      weight: 25,
-    },
-    {
-      name: "jose",
-      age: 17,
-      weight: 25,
-    },
-    {
-      name: "pedro",
-      age: 32,
-      weight: 25,
-    },
-    {
-      name: "ruth",
-      age: 22,
-      weight: 25,
-    },
-  ];
+const COLORS = [
+  "#ce93d8",
+  "#5c6bc0",
+  "#b39ddb",
+  "#4dd0e1",
+  "#f48fb1",
+  "#d500f9",
+];
 
+const GraficoBarra = ({ data }) => {
   return (
-    <div style={{ width: "100%", height: 300 }}>
-      <ResponsiveContainer aspect={2}>
-        <BarChart data={data} width={500}>
-          <CartesianGrid strokeDasharray="4 1 1" />
+    <div style={{ height: "500px" }}>
+      <h4 className="pt-4">Cantidad Producto</h4>
+      <hr />
+      <ResponsiveContainer width="100%" aspect={2} className="pt-3">
+        <BarChart data={data} width={300}>
+          <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
           <YAxis />
           <Tooltip />
           <Legend />
-          <Bar dataKey="age" fill="#6b48ff" />
-          <Bar dataKey="weight" fill="#1ee3cf" />
+          <Bar dataKey="disponible" stackId="a" fill="#8884d8" />
+          <Bar dataKey="vendido" stackId="a" fill="#82ca9d" />
         </BarChart>
       </ResponsiveContainer>
     </div>
   );
 };
 
-const GraficaTorta = () => {
-  const data = [
-    { name: "Group A", value: 2400 },
-    { name: "Group B", value: 4567 },
-    { name: "Group C", value: 1398 },
-    { name: "Group D", value: 9800 },
-    { name: "Group E", value: 3908 },
-    { name: "Group F", value: 4800 },
-  ];
-
-  const COLORS = [
-    "#ce93d8",
-    "#5c6bc0",
-    "#b39ddb",
-    "#4dd0e1",
-    "#f48fb1",
-    "#d500f9",
-  ];
-
+const GraficaTorta = ({ data }) => {
   return (
     <div style={{ width: "100%", height: 300 }}>
+      <h4>Producto vendidos</h4>
+      <hr />
       <ResponsiveContainer>
         <PieChart>
           <Pie
-            dataKey="value"
+            dataKey="conteo"
             data={data}
             innerRadius={60}
             outerRadius={85}
@@ -95,26 +77,106 @@ const GraficaTorta = () => {
             ))}
           </Pie>
           <Tooltip />
-          <Legend />
+          <Legend layout="vetical" verticalAlign="middle" align="right" />
         </PieChart>
       </ResponsiveContainer>
     </div>
   );
 };
 
+const GraficaRadar = ({ data }) => {
+  return (
+    <div style={{ width: "100%", height: 300 }}>
+      <h4>Categoria mas vendida</h4>
+      <hr />
+      <ResponsiveContainer>
+        <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
+          <PolarGrid />
+          <PolarAngleAxis dataKey="subject" />
+          <PolarRadiusAxis />
+          <Radar
+            name="Categoria"
+            dataKey="conteo"
+            stroke="#8884d8"
+            fill="#8884d8"
+            fillOpacity={0.6}
+          />
+        </RadarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+// const GraficaLine = ({ data }) => {
+//   return (
+//     <ResponsiveContainer width="100%" height="100%" aspect={2}>
+//       <LineChart
+//         width={500}
+//         height={300}
+//         data={data}
+//         margin={{
+//           top: 5,
+//           right: 30,
+//           left: 20,
+//           bottom: 5,
+//         }}
+//       >
+//         <CartesianGrid strokeDasharray="3 3" />
+//         <XAxis dataKey="name" />
+//         <YAxis />
+//         <Tooltip />
+//         <Legend />
+//         <Line
+//           type="monotone"
+//           dataKey="disponible"
+//           stroke="#8884d8"
+//           activeDot={{ r: 8 }}
+//         />
+//         <Line type="monotone" dataKey="vendido" stroke="#82ca9d" />
+//       </LineChart>
+//     </ResponsiveContainer>
+//   );
+// };
+
+const url = process.env.REACT_APP_API_CORE_URL + "pedido/estadisticas";
+
 const Graficos = () => {
+  const [buscar, setBuscar] = useState(true);
+  const [datos, setDatos] = useState(null);
+
+  ReloadData(url, buscar, (dato) => {
+    setDatos(dato?.datos);
+    setBuscar(false);
+  });
+
   return (
     <>
       <Row className="pt-5">
-        <Col md={6}>
-          {/* Grafico de barras */}
-          <GraficoBarra />
+        <Col md={6} className="pb-5">
+          {datos?.datosCategoria && (
+            <GraficaRadar data={datos?.datosCategoria} />
+          )}
         </Col>
-        <Col md={6}>
-          {/* Grafico de Torta */}
-          <GraficaTorta />
+        <Col md={6} className="pb-5">
+          {datos?.datosProducto && <GraficaTorta data={datos?.datosProducto} />}
         </Col>
       </Row>
+      <Row className="pt-2">
+        <Col></Col>
+        <Col md={10}>
+          {datos?.datosProductoCantidad && (
+            <GraficoBarra data={datos?.datosProductoCantidad} />
+          )}
+        </Col>
+        <Col></Col>
+      </Row>
+      {/* <Row className="pt-5">
+        <Col></Col>
+        <Col md={10}>
+          <GraficaLine data={datos?.datosProductoCantidad} />
+        </Col>
+        <Col></Col>
+      </Row> */}
     </>
   );
 };
