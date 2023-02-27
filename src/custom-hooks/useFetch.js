f95536f31cc66d015122fc8f765c3f26a176d7d1
@humberto_guiracocha <humberto.guiracocha@bitekso.com>
2023-02-27 00:01:18 -0500

    se elimina codigo comentado

--- src/custom-hooks/useFetch.js
+++ src/custom-hooks/useFetch.js
@@ -28,55 +28,6 @@ const useData = (url, method, data, validar, respuesta) => {
   }, [validar, url, method, data, respuesta]);
 };
 
-// const PostData = (url, datos, validar, respuesta) => {
-//   useEffect(() => {
-//     if (validar) {
-//       const requestOptions = {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//           // Authorization: "Bearer " + localStorage.getItem("token"),
-//         },
-//         body: JSON.stringify(datos),
-//       };
-
-//       const updateData = async () => {
-//         try {
-//           const response = await fetch(url, requestOptions);
-//           const json = await response.json();
-//           respuesta(json);
-//         } catch (error) {
-//           console.log(error.toString());
-//         }
-//       };
-
-//       updateData();
-//     }
-//   }, [validar, url]);
-// };
-
-// const ReloadData = (url, validar, respuesta) => {
-//   useEffect(() => {
-//     if (validar) {
-//       const GetData = async () => {
-//         try {
-//           const response = await fetch(url, {
-//             // headers: {
-//             //   Authorization: "Bearer " + localStorage.getItem("token"),
-//             // },
-//           });
-//           const json = await response.json();
-//           respuesta(json);
-//         } catch (error) {
-//           console.log(`Error: ${error.toString()}`);
-//         }
-//       };
-
-//       GetData();
-//     }
-//   }, [validar, url, respuesta]);
-// };
-
 const PostData = (url, data, validar, respuesta) => {
   useData(url, "POST", data, validar, respuesta);
 };
